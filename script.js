b1958df22db884f3ca75e5d2ef22f0d01a7821a4
Leonardo Luiz <lcoutinholuiz@gmail.com>
2023-09-21 01:39:46 -0300

    Auto honker for nitro stream racing

---
+++ script.js
@@ -0,0 +1,12 @@
+/* Add this bit of code to the end of the script.js file */
+
+var honkInterval = window.setInterval(honkAThon, 15000);
+function honkAThon() {
+  const activeRace = document.getElementsByClassName("activeRaceItem");
+  if (activeRace){
+    const honkButton = document.getElementsByClassName("actionButton")[0];
+    if (honkButton) {
+      honkButton.click();
+    }
+  }
+}
