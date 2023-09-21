f3c2344a9af64d1c97b8296638aaa63bc72a9471
Leonardo Luiz <lcoutinholuiz@gmail.com>
2023-09-21 02:13:37 -0300

    Increasing timer to a reasonable ammount.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 /* Instructions on the README file of this repository */
 /* Add this bit of code to the end of the script.js file */
 
-
-var honkInterval = window.setInterval(honkAThon, 15000);
+/* The honk only counts once every 15 seconds or so, so a 20 seconds timer shouldn't have any issues. */
+var honkInterval = window.setInterval(honkAThon, 20000);
 function honkAThon() {
   const activeRace = document.getElementsByClassName("activeRaceItem");
   if (activeRace){
