a6d18360cdc7350f0d46f11c0653718e4fc8019f
Leonardo Luiz <lcoutinholuiz@gmail.com>
2023-09-24 12:56:13 -0300

    Enhancement

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 /* Instructions on the README file of this repository */
 /* Add this bit of code to the end of the script.js file */
 
-/* The honk only counts once every 15 seconds or so, so a 20 seconds timer shouldn't have any issues. */
-var honkInterval = window.setInterval(honkAThon, 20000);
-function honkAThon() {
-  const activeRace = document.getElementsByClassName("activeRaceItem");
-  if (activeRace){
-    const honkButton = document.getElementsByClassName("actionButton")[0];
-    if (honkButton) {
+/* The honk only counts once every 15 seconds or so, so a 17 seconds timer shouldn't have any issues. */
+setInterval(function () {
+  const activeRace = document.querySelector(".activeRaceItem");
+  if (activeRace) {
+    const honkButton = document.querySelector(".actionButton");
+    if (honkButton)
       honkButton.click();
-    }
   }
-}
+}, 17000);
