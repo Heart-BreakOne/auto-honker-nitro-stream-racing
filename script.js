d1bbc267420b567881a46b650a92e7dfc8f50a86
Leonardo Luiz <lcoutinholuiz@gmail.com>
2024-02-29 18:12:32 -0300

    Streamlining script

--- script.js
+++ script.js
@@ -1,37 +1,8 @@
 let activeRace;
-setInterval(usePowerUps, 5000);
-setInterval(honk, 17000);
-setInterval(collect, 2000);
 
-//Claims race reward
-function collect() {
-  const button = document.getElementById("claimButton");
-  if (button) {
-    button.click();
-    activeRace = false
-  }
-}
-
-// The honk only counts once every 15 seconds or so, so a 17 seconds timer shouldn't have any issues.
-function honk() {
-  if (activeRace) {
-    const honkButton = document.querySelector(".actionButton");
-    if (honkButton)
-      honkButton.click();
-  }
-}
-
-// Use all power ups and megas available
-function usePowerUps() {
+setInterval(() => document.getElementById("claimButton")?.click() && (activeRace = false), 2000);
+setInterval(() => activeRace && document.querySelector(".actionButton")?.click(), 17000);
+setInterval(() => {
   activeRace = document.querySelector(".activeRaceItem");
-  if (activeRace) {
-    const megaButton = document.querySelector('.ml-4');
-    if (megaButton) {
-      megaButton.click();
-    }
-    const powerUpButtons = document.querySelectorAll('.cursor-pointer.font-bold');
-    powerUpButtons.forEach(button => {
-      button.click();
-    });
-  }
-}
+  activeRace && document.querySelectorAll('.ml-4, .cursor-pointer.font-bold').forEach(button => button.click());
+}, 5000);
